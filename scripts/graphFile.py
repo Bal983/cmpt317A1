039a5e47e5047caa416100937a7f114f7ef875e9
Bryton Leason (bal983) <bryton.leason@usask.ca>
2017-01-20 15:38:13 -0600

    removed the ; so that it's more proper python

--- scripts/graphFile.py
+++ scripts/graphFile.py
@@ -6,80 +6,80 @@ import random
 #functions
 def initGraph( graph ):
     #adding the nodes to make a 3 by 3 graph
-    graph.add_node("1,1", pos=(1,1));
-    graph.add_node("1,2", pos=(1,2));
-    graph.add_node("1,3", pos=(1,3));
-    graph.add_node("1,4", pos=(1,4));
-    graph.add_node("2,1", pos=(2,1));
-    graph.add_node("2,2", pos=(2,2));
-    graph.add_node("2,3", pos=(2,3));
-    graph.add_node("2,4", pos=(2,4));
-    graph.add_node("3,1", pos=(3,1));
-    graph.add_node("3,2", pos=(3,2));
-    graph.add_node("3,3", pos=(3,3));
-    graph.add_node("3,4", pos=(3,4));
-    graph.add_node("4,1", pos=(4,1));
-    graph.add_node("4,2", pos=(4,2));
-    graph.add_node("4,3", pos=(4,3));
-    graph.add_node("4,4", pos=(4,4));
+    graph.add_node("1,1", pos=(1,1))
+    graph.add_node("1,2", pos=(1,2))
+    graph.add_node("1,3", pos=(1,3))
+    graph.add_node("1,4", pos=(1,4))
+    graph.add_node("2,1", pos=(2,1))
+    graph.add_node("2,2", pos=(2,2))
+    graph.add_node("2,3", pos=(2,3))
+    graph.add_node("2,4", pos=(2,4))
+    graph.add_node("3,1", pos=(3,1))
+    graph.add_node("3,2", pos=(3,2))
+    graph.add_node("3,3", pos=(3,3))
+    graph.add_node("3,4", pos=(3,4))
+    graph.add_node("4,1", pos=(4,1))
+    graph.add_node("4,2", pos=(4,2))
+    graph.add_node("4,3", pos=(4,3))
+    graph.add_node("4,4", pos=(4,4))
 
     #adding some edges into the graph
-    graph.add_edge("2,2", "3,2");
-    graph.add_edge("2,2", "1,2");
-    graph.add_edge("2,2", "2,1");
-    graph.add_edge("2,2", "2,3");
-    graph.add_edge("2,1", "3,1");
-    graph.add_edge("3,1", "3,2");
-    graph.add_edge("3,2", "3,3");
-    graph.add_edge("2,3", "1,3");
-    graph.add_edge("1,3", "1,2");
-    graph.add_edge("1,2", "1,1");
-    graph.add_edge("1,3", "1,4");
-    graph.add_edge("2,3", "2,4");
-    graph.add_edge("3,3", "3,4");
-    graph.add_edge("3,3", "4,3");
-    graph.add_edge("3,2", "4,2");
-    graph.add_edge("4,2", "4,1");
-    graph.add_edge("4,3", "4,4");
-    graph.add_edge("3,4", "4,4");
+    graph.add_edge("2,2", "3,2")
+    graph.add_edge("2,2", "1,2")
+    graph.add_edge("2,2", "2,1")
+    graph.add_edge("2,2", "2,3")
+    graph.add_edge("2,1", "3,1")
+    graph.add_edge("3,1", "3,2")
+    graph.add_edge("3,2", "3,3")
+    graph.add_edge("2,3", "1,3")
+    graph.add_edge("1,3", "1,2")
+    graph.add_edge("1,2", "1,1")
+    graph.add_edge("1,3", "1,4")
+    graph.add_edge("2,3", "2,4")
+    graph.add_edge("3,3", "3,4")
+    graph.add_edge("3,3", "4,3")
+    graph.add_edge("3,2", "4,2")
+    graph.add_edge("4,2", "4,1")
+    graph.add_edge("4,3", "4,4")
+    graph.add_edge("3,4", "4,4")
 
-    graph.nodes(data=True);
+    graph.nodes(data=True)
 
 
 def drawGraph( graph, colour):
     #creating the axis
-    plot.ylim([0,5]);
-    plot.xlim([0,5]);
+    plot.ylim([0,5])
+    plot.xlim([0,5])
     
     #setting some options
-    plot.title("Initial Graph");
+    plot.title("Initial Graph")
     
     #getting the positions
-    pos = graphLibrary.get_node_attributes(graph, 'pos');
+    pos = graphLibrary.get_node_attributes(graph, 'pos')
     
     #drawing the graph
-    graphLibrary.draw_networkx(graph, pos, font_size=12, node_color=colour, node_size=600, width=2.0);
+    graphLibrary.draw_networkx(graph, pos, font_size=12, node_color=colour, node_size=600, width=2.0)
 
     #colour specific for the garages, pickup points and dropoff points
-    plot.show();
+    plot.show()
 
 def createPoints( numberOfPackages, numberOfGarages, graph):
     #generating random locations for the garage and the package
-    garageNumber = random.randrange(0, len(list(graph.nodes())));
-    packagePickupNumber = random.randrange(0, len(list(graph.nodes())));
-    packageDropoffNumber = random.randrange(0, len(list(graph.nodes())));
+    garageNumber = random.randrange(0, len(list(graph.nodes())))
+    packagePickupNumber = random.randrange(0, len(list(graph.nodes())))
+    packageDropoffNumber = random.randrange(0, len(list(graph.nodes())))
         
     while ((packageDropoffNumber == packagePickupNumber)):
-        packageDropoffNumber = random.randrange(1, len(list(graph.nodes())));
+        packageDropoffNumber = random.randrange(1, len(list(graph.nodes())))
 
     return [garageNumber, packagePickupNumber, packageDropoffNumber]
 
 def testing():
     #setting up staticGraph
-    staticGraph = graphLibrary.Graph();
-    initGraph(staticGraph);
+    staticGraph = graphLibrary.Graph()
+    initGraph(staticGraph)
     
     #testing the graph
-    createPoints( 1, 1, staticGraph);
-    drawGraph( staticGraph, 'white' );
+    createPoints( 1, 1, staticGraph)
+    drawGraph( staticGraph, 'white' )
 
