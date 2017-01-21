f810ff6828ee58b5e7838e80f7b45ab2b3e88740
Parker Neufeld <parker.neufeld@gmail.com>
2017-01-21 17:14:36 -0600

    Still contains bugs. Currently has an error related to node positions in graphFile.

--- scripts/graphFile.py
+++ scripts/graphFile.py
@@ -40,46 +40,45 @@ def printGraphStats( toPrint ):
 
 def initPresetGraph1( graph ):
     #adding the nodes to make a 3 by 3 graph
-    graph.add_node("1,1", pos=(1,1))
-    graph.add_node("1,2", pos=(1,2))
-    graph.add_node("1,3", pos=(1,3))
-    graph.add_node("1,4", pos=(1,4))
-    graph.add_node("2,1", pos=(2,1))
-    graph.add_node("2,2", pos=(2,2))
-    graph.add_node("2,3", pos=(2,3))
-    graph.add_node("2,4", pos=(2,4))
-    graph.add_node("3,1", pos=(3,1))
-    graph.add_node("3,2", pos=(3,2))
-    graph.add_node("3,3", pos=(3,3))
-    graph.add_node("3,4", pos=(3,4))
-    graph.add_node("4,1", pos=(4,1))
-    graph.add_node("4,2", pos=(4,2))
-    graph.add_node("4,3", pos=(4,3))
-    graph.add_node("4,4", pos=(4,4))
+    graph.add_node("1,1", pos=(1,1));
+    graph.add_node("1,2", pos=(1,2));
+    graph.add_node("1,3", pos=(1,3));
+    graph.add_node("1,4", pos=(1,4));
+    graph.add_node("2,1", pos=(2,1));
+    graph.add_node("2,2", pos=(2,2));
+    graph.add_node("2,3", pos=(2,3));
+    graph.add_node("2,4", pos=(2,4));
+    graph.add_node("3,1", pos=(3,1));
+    graph.add_node("3,2", pos=(3,2));
+    graph.add_node("3,3", pos=(3,3));
+    graph.add_node("3,4", pos=(3,4));
+    graph.add_node("4,1", pos=(4,1));
+    graph.add_node("4,2", pos=(4,2));
+    graph.add_node("4,3", pos=(4,3));
+    graph.add_node("4,4", pos=(4,4));
 
     #adding some edges into the graph
-    graph.add_edge("2,2", "3,2")
-    graph.add_edge("2,2", "1,2")
-    graph.add_edge("2,2", "2,1")
-    graph.add_edge("2,2", "2,3")
-    graph.add_edge("2,1", "3,1")
-    graph.add_edge("3,1", "3,2")
-    graph.add_edge("3,2", "3,3")
-    graph.add_edge("2,3", "1,3")
-    graph.add_edge("1,3", "1,2")
-    graph.add_edge("1,2", "1,1")
-    graph.add_edge("1,3", "1,4")
-    graph.add_edge("2,3", "2,4")
-    graph.add_edge("3,3", "3,4")
-    graph.add_edge("3,3", "4,3")
-    graph.add_edge("3,2", "4,2")
-    graph.add_edge("4,2", "4,1")
-    graph.add_edge("4,3", "4,4")
-    graph.add_edge("3,4", "4,4")
+    graph.add_edge("2,2", "3,2");
+    graph.add_edge("2,2", "1,2");
+    graph.add_edge("2,2", "2,1");
+    graph.add_edge("2,2", "2,3");
+    graph.add_edge("2,1", "3,1");
+    graph.add_edge("3,1", "3,2");
+    graph.add_edge("3,2", "3,3");
+    graph.add_edge("2,3", "1,3");
+    graph.add_edge("1,3", "1,2");
+    graph.add_edge("1,2", "1,1");
+    graph.add_edge("1,3", "1,4");
+    graph.add_edge("2,3", "2,4");
+    graph.add_edge("3,3", "3,4");
+    graph.add_edge("3,3", "4,3");
+    graph.add_edge("3,2", "4,2");
+    graph.add_edge("4,2", "4,1");
+    graph.add_edge("4,3", "4,4");
+    graph.add_edge("3,4", "4,4");
 
-    graph.nodes(data=True)
+    graph.nodes(data=True);
 
-<<<<<<< HEAD
 def makeAllFigures( color):
     plot.figure(0)
     count = 0
@@ -91,41 +90,33 @@ def makeAllFigures( color):
     
     drawGraph (graph , color , 10 , 10
     plot.xlim([0,x]);
-=======
-
-def drawGraph( graph, colour):
-    #creating the axis
-    plot.ylim([0,5])
-    plot.xlim([0,5])
->>>>>>> 039a5e47e5047caa416100937a7f114f7ef875e9
     
     #setting some options
-    plot.title("Initial Graph")
+    plot.title("Initial Graph");
     
     #getting the positions
-    pos = graphLibrary.get_node_attributes(graph, 'pos')
+    pos = graphLibrary.get_node_attributes(graph, 'pos');
     
     #drawing the graph
-    graphLibrary.draw_networkx(graph, pos, font_size=12, node_color=colour, node_size=600, width=2.0)
+    graphLibrary.draw_networkx(graph, pos, font_size=12, node_color=colour, node_size=600, width=2.0);
 
     #colour specific for the garages, pickup points and dropoff points
-    plot.show()
+    plot.show();
 
 def createPoints( numberOfPackages, numberOfGarages, graph):
     #generating random locations for the garage and the package
-    garageNumber = random.randrange(0, len(list(graph.nodes())))
-    packagePickupNumber = random.randrange(0, len(list(graph.nodes())))
-    packageDropoffNumber = random.randrange(0, len(list(graph.nodes())))
+    garageNumber = random.randrange(0, len(list(graph.nodes())));
+    packagePickupNumber = random.randrange(0, len(list(graph.nodes())));
+    packageDropoffNumber = random.randrange(0, len(list(graph.nodes())));
         
     while ((packageDropoffNumber == packagePickupNumber)):
-        packageDropoffNumber = random.randrange(1, len(list(graph.nodes())))
+        packageDropoffNumber = random.randrange(1, len(list(graph.nodes())));
 
     return [garageNumber, packagePickupNumber, packageDropoffNumber]
 
 def testing():
     makeGraphList( 10 )
     #setting up staticGraph
-<<<<<<< HEAD
     staticGraph = graphLibrary.Graph();
     initGraph(staticGraph);
     #plot.figure(2):
@@ -136,12 +127,4 @@ def testing():
         drawGraph (graphs[i, "green"], 50, 50)
     drawGraph(staticGraph , "purple" , 10 , 10);
 
-=======
-    staticGraph = graphLibrary.Graph()
-    initGraph(staticGraph)
-    
-    #testing the graph
-    createPoints( 1, 1, staticGraph)
-    drawGraph( staticGraph, 'white' )
->>>>>>> 039a5e47e5047caa416100937a7f114f7ef875e9
 
--- scripts/search.py
+++ scripts/search.py
@@ -4,13 +4,13 @@ from collections import deque
 
 #functions
 def depthFirstSearch(graphToSearch, startNode, endNode):
-    print("DFS called")
-    print "Start Node: " + str(startNode) + "; End Node: " + str(endNode)
+    print("DFS called");
+    print "Start Node: " + str(startNode) + "; End Node: " + str(endNode);
 
     #setting up the variables needed to do the search
-    currentNode = startNode
-    toVisitStack = [currentNode]
-    nodesVisited = []
+    currentNode = startNode;
+    toVisitStack = [currentNode];
+    nodesVisited = [];
     
     #adding a counter for call comparison purposes
     counter = 0;
@@ -18,79 +18,79 @@ def depthFirstSearch(graphToSearch, startNode, endNode):
     #the actual searching loop
     while( len(toVisitStack) != 0 ):
         #this print statement just prints the stack of nodes we need to visit for debugging purposes
-        print "Stack of nodes to visit: " + str(toVisitStack)
-        counter += 1
+        print "Stack of nodes to visit: " + str(toVisitStack);
+        counter += 1;
         
         #we are currently going to visit the last item added onto the stack
-        currentNode = toVisitStack.pop()
+        currentNode = toVisitStack.pop();
         #then we need to add the current node into the list of nodes we have already visisted
-        nodesVisited.append(currentNode)
+        nodesVisited.append(currentNode);
 
         #nodes to add is simply a list of all of the neighbors of the current node
-        nodesToAdd = list(graphLibrary.all_neighbors(graphToSearch, currentNode))
+        nodesToAdd = list(graphLibrary.all_neighbors(graphToSearch, currentNode));
         
         #the first bit of heuristical logic, if one of the current neighbors are the node we are looking for, we instantly move there
         if endNode in nodesToAdd:
-            currentNode = endNode
-            nodesVisited.append(currentNode)
-            break
+            currentNode = endNode;
+            nodesVisited.append(currentNode);
+            break;
 
         #We still aren't at the location, but we don't want duplicates, so we check if anything in nodesVisited is a node we are considering to add, if we find a duplicate we remove it.
         for item in nodesVisited:
             if item in nodesToAdd:
-                nodesToAdd.remove(item)
+                nodesToAdd.remove(item);
     
         #then we add all of the neighbors onto the stack
-        toVisitStack.extend(nodesToAdd)
+        toVisitStack.extend(nodesToAdd);
 
-    print "Number of nodes explored: " + str(counter)
+    print "Number of nodes explored: " + str(counter);
     print
     return currentNode
 
 def breadthFirstSearch(graphToSearch, startNode, endNode):
-    print("BFS called")
-    print "Start Node: " + str(startNode) + "; End Node: " + str(endNode)
+    print("BFS called");
+    print "Start Node: " + str(startNode) + "; End Node: " + str(endNode);
 
     #setting up the variables needed to do the search
-    currentNode = startNode
-    toVisitQueue = deque([currentNode])
-    nodesVisited = []
+    currentNode = startNode;
+    toVisitQueue = deque([currentNode]);
+    nodesVisited = [];
     
     #adding a counter for call comparison purposes
-    counter = 0
+    counter = 0;
 
     #the actual searching loop
     while (len(toVisitQueue) != 0):
         #this print statement just prints the queue of nodes we need to visit for debugging purposes
-        print "Stack of nodes to visit: " + str(toVisitQueue)
-        counter += 1
+        print "Stack of nodes to visit: " + str(toVisitQueue);
+        counter += 1;
         
         #we are currently going to visit the first item in the queue
-        currentNode = toVisitQueue.popleft()
+        currentNode = toVisitQueue.popleft();
         #then we need to add the current node into the list of nodes we have already visisted
-        nodesVisited.append(currentNode)
+        nodesVisited.append(currentNode);
     
         #nodes to add is simply a list of all of the neighbors of the current node
-        nodesToAdd = list(graphLibrary.all_neighbors(graphToSearch, currentNode))
+        nodesToAdd = list(graphLibrary.all_neighbors(graphToSearch, currentNode));
         
         #the first bit of heuristical logic, if one of the current neighbors are the node we are looking for, we instantly move there
         if endNode in nodesToAdd:
-            currentNode = endNode
-            nodesVisited.append(currentNode)
-            break
+            currentNode = endNode;
+            nodesVisited.append(currentNode);
+            break;
 
         #We still aren't at the location, but we don't want duplicates, so we check if anything in nodesVisited is a node we are considering to add, if we find a duplicate we remove it.
         for item in nodesVisited:
             if item in nodesToAdd:
-                nodesToAdd.remove(item)
+                nodesToAdd.remove(item);
     
         #then we add all of the neighbors into the stack
-        toVisitQueue.extend(nodesToAdd)
+        toVisitQueue.extend(nodesToAdd);
 
-    print "Number of Nodes explored: " + str(counter)
+    print "Number of Nodes explored: " + str(counter);
     print
     return currentNode
 
 
 def aStarSearch(graphToSearch, startNode):
-    print("A* search called")
+    print("A* search called");
