26933b0ac7481ace76fbe32341d706921efd3c70
Parker Neufeld <parker.neufeld@gmail.com>
2017-01-22 00:33:50 -0600

    Function to remove edges from any graph has been written and lightly tested. It can recieve a probability of erasing an edge and a minimum degree to maintain.

--- scripts/4Grid.py
+++ scripts/4Grid.py
@@ -2,6 +2,7 @@
 Draw a graph with matplotlib.
 You must have matplotlib for this to work.
 """
+from random import randrange
 __author__ = """Aric Hagberg (hagberg@lanl.gov)"""
 #    Copyright (C) 2004-2015
 #    Aric Hagberg <hagberg@lanl.gov>
@@ -10,12 +11,55 @@ __author__ = """Aric Hagberg (hagberg@lanl.gov)"""
 #    All rights reserved.
 #    BSD license.
 
+import random
+
 try:
     import matplotlib.pyplot as plt
 except:
     raise
 
 import networkx as nx
+
+
+def minimumSpanningTree( G ):
+    trim(G , 1.0 , 1.0)
+def trim( G , minDegree , likelyHood):
+    #All edges retrieved from the graph
+	#
+    print("Trimming" + str(G))
+    print ("Nodes :" + str(G.nodes(True)))
+    for n in G.nodes():
+        print("Processing node: " + str(n))
+         #Each edge is initially valid to remove
+
+
+        neighbours = nx.to_edgelist(G,n)
+        print("List of edges attached to " + str(n) + " : " + str( neighbours))
+        edgesToNeighbours = []
+        for e in neighbours:
+
+             # edgesToNeighbours.append( (e[0],e[1],))
+
+
+
+             if(G.degree(n) > minDegree):
+
+                for adjacent in edgesToNeighbours:
+
+
+                    #valid = (G.degree(adjacent) > minDegree  or   G.degree(n) > minDegree)
+                    valid = (G.degree(n) > minDegree)
+
+                    print  (  (likelyHood) >= random.random())
+                    if( valid):
+
+                        print("removing" + str())
+
+                        G.remove_edge(e )
+                        print ("Nodes :" + str(G.nodes(False)))
+
+
+'''
 G=nx.grid_2d_graph(4,4)  #4x4 grid
 
 pos=nx.spring_layout(G,iterations=100)
@@ -24,7 +68,7 @@ plt.subplot(221)
 nx.draw(G,pos,font_size=8)
 
 plt.subplot(222)
-nx.draw(G,pos,node_color='k',node_size=0,with_labels=False)
+nx.draw(G,pos,node_color='k',node_size=0,with_labels=True)
 
 plt.subplot(223)
 nx.draw(G,pos,node_color='g',node_size=250,with_labels=False,width=6)
@@ -35,10 +79,8 @@ nx.draw(H,pos,node_color='b',node_size=20,with_labels=False)
 
 plt.savefig("four_grids.png")
 plt.show()
-
-
-
-######################
+'''
+                        ######################
 #Another grid, but this one can be anysize
 
 
@@ -47,12 +89,6 @@ try:
     import matplotlib.pyplot as pltGrid
 except:
     raise
-
-
-
-
-
-
 N = 10
 G=nx.grid_2d_graph(N,N)
 pos = dict( (n, n) for n in G.nodes() )
@@ -61,3 +97,20 @@ nx.draw_networkx(G, pos=pos, labels=labels)
 
 pltGrid.axis('off')
 pltGrid.show()
+
+
+
+
+N = 3#input("Enter dimensions for grid plot.")
+G=nx.grid_2d_graph(N,N)
+trim( G , 1.0 , 1.0)
+minimumSpanningTree(G)
+pos = dict( (n, n) for n in G.nodes() )
+labels = dict( ((i, j), i * 10 + j) for i, j in G.nodes() )
+nx.draw_networkx(G, pos=pos, labels=labels)
+pltGrid.axis('off')
+pltGrid.show()
+
+
+
+
