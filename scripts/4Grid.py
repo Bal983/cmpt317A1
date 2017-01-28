64943e94a0d0da2a944c41abe3b4e1bb428b7205
Parker Neufeld <parker.neufeld@gmail.com>
2017-01-28 16:58:28 -0600

    Random edge removal rough draft working visually in 4grid.py

--- scripts/4Grid.py
+++ scripts/4Grid.py
@@ -6,12 +6,12 @@ __author__ = """Aric Hagberg (hagberg@lanl.gov)"""
 #    All rights reserved.
 #    BSD license.
 
-#Imports
+# Imports
 try:
     import math
-    from ast import literal_eval    #additional library to fix a string interpretat
+    from ast import literal_eval  # additional library to fix a string interpretat
     import matplotlib.pyplot as plt
-    import graphFile
+    import graphImplementation
     from networkx.generators.classic import empty_graph, complete_graph, grid_graph
 
 except:
@@ -19,37 +19,38 @@ except:
 import networkx as nx
 import random
 
-#functions
-def minimumSpanningTree( G ):
+# functions
+def minimumSpanningTree(G):
+
+    print "Making a minimum spanning tree."
     trim(G , 1.0 , 1.0)
-    
-def trim( G , minDegree , likelyHood):
-    #All edges retrieved from the graph
-    print("Trimming" + str(G))
+    print "Remaining edges"
+def gatherLegalSample(graph, reductionFactor, minimumDegree):
+    originalNodes = graph.nodes()
+    sample = random.sample(originalNodes, int(reductionFactor * len(originalNodes)))
+    for node in sample:
+        edges = G.edges(node)
+        for edge in edges:
+            valid = G.degree(edge[0]) > minimumDegree and G.degree(edge[1]) > minimumDegree
+            if(valid):
+                G.remove_edge(edge[0],edge[1])
+
+def trim(G , minDegree , percent):
+    # All edges retrieved from the graph
+    print("Graph has the following nodes: " + str (G.edges()))
+    print("Trimming " + str(percent) + " of the nodes" + str(G))
+    sample = random.sample(G.nodes)
+    print ()
+
     print ("Nodes :" + str(G.nodes(True)))
     for n in G.nodes():
          print("Processing node: " + str(n))
-         #Each edge is initially valid to remove
-         neighbours = nx.to_edgelist(G,n)
-         print("List of edges attached to " + str(n) + " : " + str( neighbours))
-         edgesToNeighbours = []
-         for e in neighbours:
-             # edgesToNeighbours.append( (e[0],e[1],))
-             if(G.degree(n) > minDegree):
-                 #The edges to be processed
-                 for adjacent in edgesToNeighbours:
-                    #valid = (G.degree(adjacent) > minDegree  or   G.degree(n) > minDegree)
-                    # Both nodes must meet the criteria
-                    valid = (G.degree(n) > minDegree or G.degree(adjacent[1]))
-                    print( (likelyHood) >= random.random() )
+         # Each edge is initially valid to remove
+         neighbours = nx.to_edgelist(G, n)
+         print("List of edges attached to " + str(n) + " : " + str(neighbours))
 
-                    if(valid):
 
-                        print("removing" + str())
 
-                        G.remove_edge(e )
-                        print ("Nodes :" + str(G.nodes(False)))
-                        
 '''
 G=nx.grid_2d_graph(4,4)  #4x4 grid
 
@@ -72,26 +73,29 @@ plt.savefig("four_grids.png")
 plt.show()
 '''
                         ######################
-#Another grid, but this one can be anysize
+# Another grid, but this one can be anysize
 try:
     import matplotlib.pyplot as pltGrid
 except:
     raise
 N = 10
-G=nx.grid_2d_graph(N,N)
-pos = dict( (n, n) for n in G.nodes() )
-labels = dict( ((i, j), i * 10 + j) for i, j in G.nodes() )
+G = nx.grid_2d_graph(N, N)
+pos = dict((n, n) for n in G.nodes())
+labels = dict(((i, j), i * 10 + j) for i, j in G.nodes())
 nx.draw_networkx(G, pos=pos, labels=labels)
 
 pltGrid.axis('off')
 pltGrid.show()
 
-N = 3#input("Enter dimensions for grid plot.")
-G=nx.grid_2d_graph(N,N)
-trim( G , 1.0 , 1.0)
-minimumSpanningTree(G)
-pos = dict( (n, n) for n in G.nodes() )
-labels = dict( ((i, j), i * 10 + j) for i, j in G.nodes() )
+N = 10  # input("Enter dimensions for grid plot.")
+G = nx.grid_2d_graph(N, N)
+# trim(G , 2.0 , 1.0)
+gatherLegalSample(G, .5, 2)
+
+# minimumSpanningTree(G)
+
+pos = dict((n, n) for n in G.nodes())
+labels = dict(((i, j), i * 10 + j) for i, j in G.nodes())
 nx.draw_networkx(G, pos=pos, labels=labels)
 pltGrid.axis('off')
 pltGrid.show()
