7acddd3820aef29c7041c4c842e8edc52e96be50
Parker Neufeld <parker.neufeld@gmail.com>
2017-01-28 20:10:32 -0600

    Moved graph4 functions into graphImplementation.

--- scripts/blackboardController.py
+++ scripts/blackboardController.py
@@ -17,14 +17,15 @@ def main():
     numberOfCars            = 10            # N
     numberOfPackages        = 100           # K
     numberOfGraphs          = 1             # This will probably remain 1
-    
+
+
     # These values are used for the test log entry - adjust before running tests
     codeVersion             = "f5572fe"     # The first 7 characters of the run's GitHub revision code
     teamMember              = "David"       # The name of the person running this test
     packageAssignmentMethod = "Arbitrary"   # The method used to assign packages
     pathfindingMethod       = "A* Search"   # The method used to pathfind
     currentDatetime = datetime.datetime.now()
-    
+
     #Generating a list of graphs to use
     sys.stdout.write("Creating graph........................")
     graphImplementation.makeGraphList( graphSize )
@@ -36,27 +37,27 @@ def main():
             sys.stdout.write("Creating car and package objects......")
             objectList = graphImplementation.createObjects(numberOfCars, numberOfPackages, currentGraph)
             print "Done!"
-            
+
             #Note: objectList is a list of two lists formatted as follows:
                 #cars: the list of garage numbers that exist on the map
                 #packages: the list of package objects, each object knows its drop off/pickup locations
             cars = objectList[0]
             packages = objectList[1]
-            
+
             # Assign the packages to cars
             sys.stdout.write("Assigning packages to cars............")
             assignPackages(currentGraph, cars, packages)
             print "Done!"
-            
-            
+
+
             for car in cars:
                 print car.totalDifficulty
                 print len(car.packageList)
-            
+
             # Initialize timer
             grandTotal = 0
             startTime = time.time()
-            
+
             # Call a search method for each car on the map
             for car in cars:
                 print "CAR " + str(car.identifier) + " IS BEGINNING ROUTE WITH " + str(len(car.packageList)) + " PACKAGES"
@@ -71,26 +72,26 @@ def main():
             print "\tAnd the total real time taken was: " + str(endTime - startTime)
 
     print "All packages delivered!"
-    
+
     if ( graphSize <= 10):
         graphImplementation.makeAllFigures( "White" )
-        
+
     # Print diagnostic information at very end of simulation
     print "=========================================================================="
     print "EXECUTION LOG - COPY TO TESTING SPREADSHEET"
     print "--------------------------------------------------------------------------"
-    
+
     # Shouldn't need to alter these directly
-    sys.stdout.write(currentDatetime.strftime("%d-%b-%Y %I:%M %p") + "\t") 
-    sys.stdout.write(codeVersion + "\t")                              
-    sys.stdout.write(teamMember + "\t")                               
-    sys.stdout.write(str(numberOfCars) + "\t")                             
+    sys.stdout.write(currentDatetime.strftime("%d-%b-%Y %I:%M %p") + "\t")
+    sys.stdout.write(codeVersion + "\t")
+    sys.stdout.write(teamMember + "\t")
+    sys.stdout.write(str(numberOfCars) + "\t")
     sys.stdout.write(str(numberOfPackages) + "\t")
     sys.stdout.write(str(graphSize) + "\t")
     sys.stdout.write(str(numberOfGraphs) + "\t")
-    sys.stdout.write("Arbitrary" + "\t")                                    
-    sys.stdout.write("A* Search" + "\t")                                    
-    sys.stdout.write(str(endTime - startTime) + "\t")                      
+    sys.stdout.write("Arbitrary" + "\t")
+    sys.stdout.write("A* Search" + "\t")
+    sys.stdout.write(str(endTime - startTime) + "\t")
     sys.stdout.write(str(grandTotal) + "\t")
     print
     print "=========================================================================="
@@ -98,16 +99,16 @@ def main():
 #logic for assigning packages to cars in a smart way will eventually go here
 #Note: package difficulty is being calculated at the point of package creation access it using package.difficulty
 def assignPackages( graph, CARS, PACKAGES):
-    #step one, sort the list of packages based on their difficulties    
+    #step one, sort the list of packages based on their difficulties
     minHeap = []
     for package in PACKAGES:
         heapq.heappush(minHeap, (-1 * package.difficulty, package))
-        
+
     #note, in this case, the gScores will be the difficulty from any car at its current position to the package pickup location
     #the heuristic is simply the difficulty rating of the package, which is calculated at the time of package construction
     #so the fScore will be the difficulty to deliver the package + the difficulty to get to the package.
 
-    difficultyPerCar = dict()   
+    difficultyPerCar = dict()
 
     while minHeap:
         currentNode = heapq.heappop(minHeap)    #note, current node will be a tuple formated (package difficulty, package)
@@ -116,12 +117,12 @@ def assignPackages( graph, CARS, PACKAGES):
         #this loop will calculate the current gScore for every car
         for car in CARS:
             difficultyPerCar[car] = getDifficulty(car, currentNode[1]) + car.totalDifficulty
-        
+
         bestCost = min(difficultyPerCar.items(), key=lambda x: x[1])
-        
+
         bestCost[0].packageList.append(currentNode[1])
         bestCost[0].totalDifficulty += difficultyPerCar[bestCost[0]]
-        
+
         #print bestCost[0].totalDifficulty
         #now we need to assign the package to the car with the smallest fScore that also has the lowest currentDifficulty
 
--- scripts/graphImplementation.py
+++ scripts/graphImplementation.py
@@ -14,11 +14,11 @@ graphs = []                 #A list of graphs that the graphFile manages
 def makeGraphList( size ):
     #add more functions here if we want to test with a longer list of graphs.
     createGridGraph ( size )
-    
+
     #after we generate all the graphs, #print all of the stats
     #for graph in graphs:
-    #    printGraphStats( graph )      
-    
+    #    printGraphStats( graph )
+
 #a grid graph by default is such that each node has an edge between each coordinate neighbor.
 #technical the graphs can be size n by m, but n by n is more visually pleasing and doesn't take away from the searches.
 def createGridGraph ( size ):
@@ -32,7 +32,7 @@ def createGridGraph ( size ):
     #print ("Standard library stats:")
     #print (graphLibrary.info(toPrint))
     #print
-    
+
 
 #for each graph in the graphs[] list, we create a window for it and call drawGraph.
 def makeAllFigures( color ):
@@ -56,7 +56,7 @@ def drawGraph( graph, colour):
     plot.xlim([-1,(size)])
 
     pos = dict( (node, node) for node in graph.nodes() )
-        
+
     #drawing the graph
     graphLibrary.draw_networkx(graph, pos, node_color=colour, width=2.0, linewidth=2.0, font_size=10, node_size=800)
 
@@ -66,23 +66,23 @@ def createObjects( numberOfGarages, numberOfPackages, graph):
     #defining empty lists for the two groups of objects we want to return
     carList = list()
     packageList = list()
-    
+
     #generating x car objects with a random garage location
     for x in range( 0, numberOfGarages ):
         maxRange = len(list(graph.nodes()))
         garageLocation = random.randrange(0, maxRange)
         carList.append( Car(x, list(graph.nodes())[garageLocation]) )
         #print "Car " + str(x) + " has been created"
-        
+
     #generating x package objects with random pickup/dropoff locations
     #limitation: pickup and dropoff location are never the same
     for x in range( 0, numberOfPackages ):
         pickupLocation = random.randrange( 0, maxRange )
         dropoffLocation = random.randrange( 0, maxRange )
-        
+
         while( pickupLocation == dropoffLocation ):
             dropoffLocation = random.randrange( 0, maxRange )
-            
+
         packageList.append( Package(x, list(graph.nodes())[pickupLocation], list(graph.nodes())[dropoffLocation]) )
         #print "Package " + str(x) + " has been created"
 
@@ -91,6 +91,61 @@ def createObjects( numberOfGarages, numberOfPackages, graph):
 def removeRandomEdges( graph ):
     #put the code that removes random edges here.
     print "removeRandomEdges was called"
+def minimumSpanningTree(G):
+
+    print "Making a minimum spanning tree."
+    reduceGraph(G , 1.0 , 1.0)
+    print "Remaining edges"
+# reduces the connected of a graph by selecting a percentage of the nodes and destroying
+# the edges if a minimum degree is maintained
+# reductionFactor :float in (0.0 , 1.0 )
+# minimumDegree   :int in [0 , inf)
+
+def reduceGraph(graph, reductionFactor, minimumDegree):
+    print "Trimming " + str(reductionFactor * 100) + "% of the nodes in " + str(G)
+    print "Minimum Degree of " + str(minimumDegree) + " will be preserved."
+    nx.info (graph)
+    originalNodes = graph.nodes()
+    # Take a random sample of the nodes according to the parameters
+    sample = random.sample(originalNodes, int(reductionFactor * len(originalNodes)))
+    # Remove all edges that don't break the degree rule
+    edgeCountOrig = len(G.edges())
+    print "Starting edge count: " + str (edgeCountOrig)
+    for node in sample:
+        edges = G.edges(node)
+        for edge in edges:
+            valid = G.degree(edge[0]) > minimumDegree and G.degree(edge[1]) > minimumDegree
+            if(valid):
+
+                G.remove_edge(edge[0], edge[1])
+    edgeCountFinal = len(G.edges())
+    print "Starting edge count: " + str(edgeCountFinal)
+    print "Final reduction percentage :" + str( ( float (edgeCountFinal)/ float (edgeCountOrig )))
+# reduces the connected of a graph by selecting a percentage of the nodes and destroying
+# the edges if a minimum degree is maintained. Addition: pseudo-random defects compared to standard result
+# reductionFactor :float in (0.0 , 1.0 )
+# minimumDegree   :int in [0 , inf)
+# rand            :float in (0.0 , 1.0 ) : likelihood for a removal to be executed
+def reduceGraphRand(graph, reductionFactor, minimumDegree, randomFactor):
+    print "Trimming " + str(reductionFactor * 100) + "% of the nodes in " + str(G)
+    print "Minimum Degree of " + str(minimumDegree) + " will be preserved."
+    nx.info (graph)
+    originalNodes = graph.nodes()
+    # Take a random sample of the nodes according to the parameters
+    sample = random.sample(originalNodes, int(reductionFactor * len(originalNodes)))
+    # Remove all edges that don't break the degree rule
+    edgeCountOrig = len(G.edges())
+    print "Starting edge count: " + str (edgeCountOrig)
+    for node in sample:
+        edges = G.edges(node)
+        for edge in edges:
+            valid = G.degree(edge[0]) > minimumDegree and G.degree(edge[1]) > minimumDegree
+            if(valid):
+
+                G.remove_edge(edge[0], edge[1])
+    edgeCountFinal = len(G.edges())
+    print "Starting edge count: " + str(edgeCountFinal)
+    print "Final reduction percentage :" + str( ( float (edgeCountFinal)/ float (edgeCountOrig )))
 
 def testing( size ):
     print
@@ -98,7 +153,7 @@ def testing( size ):
     print "Testing the graphImplementation file"
     #testing the mackGraphList method, should create a defined amount of graphs of size size
     makeGraphList( size )
-    
+
     #this method will draw all of the above graphs with node colour red.
     makeAllFigures( "red" )
 
